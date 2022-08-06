0e2b311ea5fa0fb6f01ea706b0d9cbbeddc3e9b9
u005 <s.jeffery.ye@gmail.com>
2022-08-06 14:04:56 -0700

    file reader

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,19 @@ const Generator = () => (
   </div>
 )
 
-const nouns_list = ["a", "b", "c"];
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+  
+  return arr;
+}
+
+syncReadFile('../_.txt');
+
+const organism_list = ["a", "b", "c"];
 
 function generate() {
   return nouns_list[1];
