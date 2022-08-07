eea0c9a9a1fd67b17cafe44ecb6d0b3785e4f011
u005 <s.jeffery.ye@gmail.com>
2022-08-06 17:50:05 -0700

    added new works, working-1

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export class Home extends React.Component{
           <h1>Uncommon App</h1>
         </div>
 
-        <div class = "description">
+        <div class = "description"> 
           <p>Write the worst college essay.</p>
         </div>
 
@@ -38,12 +38,12 @@ export class Home extends React.Component{
 
 const Generator = () => (
   <div class = "generatorText">
-    <p>I've faced hardships, challenges, and failures for my entire life, and I believe that they're fundamental for future success. 
-      As { getAorAn(getSnippet(adjectives_list)) }
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I've faced hardships, challenges, and failures for my entire life, and I believe that they're fundamental for future success. 
+      As { getAorAn(getSnippet(adjectives_list)) } &nbsp;
       { getSnippet(org_list) }, my life was shaped by my experience 
-      with { getSnippet(issues_list) }. Since I was { getSnippet(scenario_list) } and my mother was run over 
+      with { getSnippet(issues_list) }. Since I was { getSnippet(scenario_list) }, and my mother was run over 
       by { getAorAn(getSnippet(vehic_list)) }, I've been living with my { getSnippet(caretaker_list) } who { getSnippet(crimes_list) } and 
-      does { getSnippet(hobby_list) } in their spare time. <br/>&nbsp;&nbsp;&nbsp;&nbsp; My life had been relatively easy up until this point, but everything changed 
+      does { getSnippet(hobby_list) } in their spare time. <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; My life had been relatively easy up until this point, but everything changed 
       when I { getSnippet(events_list) }. I was on the brink of death for { Math.random()*50 } whole { getSnippet(times_list) } before the doctors said I would pull 
       through. From that point on, I pulled my life together. I graduated high school with a { Math.floor(Math.random()*4) + "." + Math.floor(Math.random()*9) } GPA.
       I knew that I wanted to go to { getSnippet(school_list) } and become the best { getSnippet(job_list) } the world has ever seen.</p>
@@ -59,18 +59,18 @@ const times_list = ["Picoseconds", "Jiffies", "Centuries", "Millenniums", "Eras"
 const vehic_list = ["Unidentified Aerial Phenomena", "Kia Soul student driver", "hydrogen powered garbage truck", "Toyota Corolla", "model train", "little tesla sedan", "Boeing 747", "Challenger space shuttle", "Amazon Prime drone", "toy construction truck", "paper airplane", "runaway office chair", "doordash robot", "stroller holding a chihuahua"]
 const school_list = ["Harvard", "BYU", "University of Southern Alabama", "Prager 'University'", "Southeastern Bible College", "Brown", "Yale", "Trade School", "Pilates class", "Public relations training", "McDonald’s Culinary Institute", "Texas Institute of Technology", "Hell’s Kitchen Culinary School"]
 const place_list = ["Boring, Oregon", "Bugscuffle, Tennesee", "Bulls, New Zealand", "Chicken, Alaska", "Condom, France", "Dinkytown, Minnesota", "French Lick, Indiana", "Friendly, West Virginia", "Gogogogo, Madagascar", "Greasy, Oklahoma", "Hazard, Kentucky", "Hygiene, Colorado", "Kill, Ireland", "Lost, Scotland", "Lick Fork, Virginia", "My Large Intestine, Texas", "Mars", "Venus"]
-const org_list = ["b-list actor", "brain-eating amoeba", "k9 drug dog", "robotic bird spy drone", "a collective 1 billion lions", "liger", "pokemon", "capybara", "anteater", "autocrat", "e. coli bacterium", "Prophet", "Sentient alien robot", "Human being"]
+const org_list = ["b-list actor", "brain-eating amoeba", "k9 drug dog", "robotic bird spy drone", "biblical angel", "liger", "pokemon", "capybara", "anteater", "autocrat", "e. coli bacterium", "Prophet", "Sentient alien robot", "Human being"]
 const job_list = ["telemarketer", "computer scientist", "HR Director", "humidifier technician", "anime illustrator", "teacher", "child soldier", "president of local golf club", "revolutionary", "abstract artist", "guillotine operator", "executioner", "pirate", "YouTuber", "Twitch streamer", "groupie", "professional cuddler", "online dating ghostwriter", "hippotherapist", "snake milker", "paper towel sniffer", "waterslide tester", "bingo manager", "feng shui consultant", "dog food taster", "drying paint watcher", "full-time netflix viewer", "iceberg mover"]
-const scenario_list = ["left on the side of the road in the middle of the Mojave Desert", "sold to cannibals living in the Canadian wilderness", "gifted to the cult of the sun", "saved from a squirrel attack that claimed the lives of my father and multiple bystanders", "was brought via stork to the Wendy's on 4th and 9th"];
+const scenario_list = ["reported my parents to the CIA for un-american activity","dropped on my thick head as a baby, causing an explosion with the equivalent energy of 1.2 megatons of TNT", "left on the side of the road in the middle of the Mojave Desert", "sold to cannibals living in the Canadian wilderness", "gifted to the cult of the sun", "saved from a squirrel attack that claimed the lives of my father and multiple bystanders", "was brought via stork to the Wendy's on 4th and 9th"];
 const hobby_list = ["crochet", "extreme sidewalk mountain biking", "long distance ultimate frisbee", "underwater football", "underwater basket weaving", "children's karate", "weeaboo katana practice", "potion brewing with the local wizard", "ceramic bird shooting with high explosives", "Texas ice fishing"]
 const caretaker_list = ["uncle", "orphanage overseer", "labor camp warden", "local governer", "state-mandated carrier pigeon", "fellow communist revolutionary", "district representative"]
 function getAorAn(input) {
   const str = input;
-  if(str.charAt(0) == 'a' || str.charAt(0) == 'e' || str.charAt(0) == 'i' || str.charAt(0) == 'o' || str.charAt(0) == 'u' ) {
-    return "an " + input + " ";
+  if(str.charAt(0) === 'a' || str.charAt(0) === 'e' || str.charAt(0) === 'i' || str.charAt(0) === 'o' || str.charAt(0) === 'u' ) {
+    return "an " + input + "";
   }
   else {
-    return "a " + input + " ";
+    return "a " + input + "";
   }
 }
 
